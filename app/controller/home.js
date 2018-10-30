e6900b243a280a5568cc3bb26cf303c165f6c888
dygapp <dygapp@qq.com>
2018-10-30 12:24:09 +0800

    upgrade

--- app/controller/home.js
+++ app/controller/home.js
@@ -9,8 +9,13 @@ class HomeController extends Controller {
   }
 
   async test() {
-    console.log(_.get(this.ctx.service, 'api.jobfairGlobal'));
-    this.ctx.body = '请通过服务接口进行调用';
+    // console.log(_.get(this.ctx.service, 'api.jobfairGlobal'));
+    // this.ctx.body = '请通过服务接口进行调用';
+    this.ctx.body = this.ctx.query;
+    console.log(_.get(this.ctx.query, 'corp.id'));
+    const data = { corp: { id: 1, name: 'aaa' } };
+    console.log(_.get(data, 'corp.id'));
+
   }
 }
 
