f852a27913c29f45e587c4dbd8cbee08ba15ba65
dygapp <dygapp@qq.com>
2019-03-15 07:40:05 +0800

    add review

--- app/service/admin/campus.js
+++ app/service/admin/campus.js
@@ -1,11 +1,38 @@
 'use strict';
 
+const assert = require('assert');
+const _ = require('lodash');
 const { CrudService } = require('naf-framework-mongoose/lib/service');
+const { BusinessError, ErrorCode } = require('naf-core').Error;
+const { isNullOrUndefined } = require('naf-core').Util;
+const { CampusTalkStatus } = require('../../util/constants');
 
 class CampusService extends CrudService {
   constructor(ctx) {
     super(ctx);
     this.model = this.ctx.model.Campus;
+  }
+
+  // 审核申请
+  async review({ id }, { status }) {
+    // console.log(params);
+    assert(id, 'id不能为空');
+    assert(!isNullOrUndefined(status), 'status不能为空');
+    assert(status === CampusTalkStatus.NORMAL || status === CampusTalkStatus.REJECT, 'status无效');
+
+    // 查询数据
+    const entity = await this.model.findById(id).exec();
+    if (isNullOrUndefined(entity)) throw new BusinessError(ErrorCode.DATA_NOT_EXIST);
+    if (entity.status !== CampusTalkStatus.PENDING && entity.status !== CampusTalkStatus.REJECT) {
+      throw new BusinessError(ErrorCode.SERVICE_FAULT, '数据状态无效');
+    }
+
+    // TODO: 保存数据
+    entity.status = status;
+    entity.unit = this.tenant;
+    await entity.save();
+
+    return entity;
   }
 
 }
